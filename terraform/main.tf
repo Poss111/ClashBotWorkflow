35761378346809b47118ce10983566240c789076
Poss111 <rixxroid@gmail.com>
2023-09-02 11:31:24 -0500

    Adding step function to iac code

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,3 +39,35 @@ module "dynamodb_table" {
     }
   ]
 }
+
+module "step_function" {
+  source = "terraform-aws-modules/step-functions/aws"
+
+  name       = "retrieve-teams"
+  definition = <<EOF
+  {
+    "Comment": "A Hello World example of the Amazon States Language using Pass states",
+    "StartAt": "Hello",
+    "States": {
+      "Hello": {
+        "Type": "Pass",
+        "Result": "Hello",
+        "Next": "World"
+      },
+      "World": {
+        "Type": "Pass",
+        "Result": "World",
+        "End": true
+      }
+    }
+  }
+  EOF
+
+  service_integrations = {
+    dynamodb = {
+      dynamodb = [module.dynamodb_table.dynamodb_table_arn]
+    }
+  }
+
+  type = "STANDARD"
+}
