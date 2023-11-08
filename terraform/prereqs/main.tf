c1f3dc17bd600228dd052a70b513009bed1ac4db
Daniel Poss <dpatposs@msn.com>
2023-11-07 19:55:22 -0600

    Adding in policy for s3 bucket

--- terraform/prereqs/main.tf
+++ terraform/prereqs/main.tf
@@ -1,3 +1,5 @@
+data "aws_caller_identity" "current" {}
+
 module "lambda_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
@@ -10,4 +12,21 @@ module "lambda_bucket" {
   versioning = {
     enabled = false
   }
+
+  policy = <<POLICY
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "AllowPutObject",
+        "Effect": "Allow",
+        "Principal": {
+          "AWS": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:ClashBotGitHubUser"
+        },
+        "Action": "s3:PutObject",
+        "Resource": "arn:aws:s3:::${var.s3_bucket_name}-${var.environment}/*"
+      }
+    ]
+  }
+  POLICY
 }
