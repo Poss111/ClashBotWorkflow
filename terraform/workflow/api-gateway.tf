d11fd39227596f616ddef1d17f8fa72edaba3c9b
Daniel Poss <rixxroid@gmail.com>
2023-11-13 20:41:22 -0600

    Updating to include a direct path for apigateway

--- terraform/workflow/api-gateway.tf
+++ terraform/workflow/api-gateway.tf
@@ -1,23 +1,3 @@
-provider "aws" {
-  region = var.region
-
-  default_tags {
-    tags = {
-      Application = "ClashBot-Workflow"
-      Environment = var.environment
-    }
-  }
-}
-
-data "aws_acm_certificate" "issued" {
-  domain   = "clash-bot.ninja"
-  statuses = ["ISSUED"]
-}
-
-resource "aws_cloudwatch_log_group" "api_gateway_default_log_group" {
-  name = "api_gateway_default_log_group-${var.environment}"
-}
-
 module "api_gateway" {
   source = "terraform-aws-modules/apigateway-v2/aws"
 
@@ -41,11 +21,20 @@ module "api_gateway" {
 
   # Routes and integrations
   integrations = {
-    "POST /api/v2" = {
+    "POST /api/v2/teams" = {
       lambda_arn       = aws_lambda_function.event_publisher_lambda.arn,
       integration_type = "AWS_PROXY"
     }
   }
+}
+
+data "aws_acm_certificate" "issued" {
+  domain   = "clash-bot.ninja"
+  statuses = ["ISSUED"]
+}
+
+resource "aws_cloudwatch_log_group" "api_gateway_default_log_group" {
+  name = "api_gateway_default_log_group-${var.environment}"
 }
 
 module "dynamodb_table" {
