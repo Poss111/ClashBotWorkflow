495360e39e5ab91712c77b68b3d3fd097efbcfd4
Daniel Poss <rixxroid@gmail.com>
2023-11-13 21:22:37 -0600

    Replacing workflow iac

--- terraform/workflow/providers.tf
+++ terraform/workflow/providers.tf
@@ -1,9 +1,10 @@
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
 
   default_tags {
     tags = {
       Application = "ClashBot-Workflow"
+      Environment = var.environment
     }
   }
 }
