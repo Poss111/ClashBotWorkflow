ac88c3e2f0042fed88294d87c69854f67eac06e6
Daniel Poss <rixxroid@gmail.com>
2023-11-17 19:20:10 -0600

    Adding in custom script for retrieving latest artifact version

--- scripts/pull-latest-artifact.sh
+++ scripts/pull-latest-artifact.sh
@@ -5,15 +5,24 @@ ARTIFACT_NAME=${2}
 ENVIRONMENT=${3}
 
 # List objects in the S3 bucket path
-OBJECTS=$(aws s3 ls s3://$BUCKET_NAME//artifacts/$ARTIFACT_NAME/$ENVIRONMENT --recursive --profile=Master | awk '{print $4}')
+OBJECTS=$(aws s3 ls s3://$BUCKET_NAME/artifacts/$ARTIFACT_NAME/$ENVIRONMENT --recursive --profile=Master | awk '{print $4}')
 
-# Extract numbers from the object names and find the maximum
-MAX_NUMBER=0
+# Extract numbers from the object names and find the maximum number artifact
+ARTIFACT_PATH=""
 for object in $OBJECTS; do
   NUMBER=$(basename $object | grep -o -E '[0-9]+')
   if [[ "$NUMBER" -gt "$MAX_NUMBER" ]]; then
-    MAX_NUMBER=$NUMBER
+    ARTIFACT_PATH=$object
   fi
 done
 
-echo "Maximum number: $MAX_NUMBER"
+# Check if ARTIFACT_PATH is empty
+if [[ -z "$ARTIFACT_PATH" ]]; then
+  echo "Error: No artifact path found" >&2
+  exit 1
+fi
+
+echo "PATH: $ARTIFACT_PATH"
+
+# Set ARTIFACT_PATH as an output
+echo "::set-output name=artifact-path::$ARTIFACT_PATH"
